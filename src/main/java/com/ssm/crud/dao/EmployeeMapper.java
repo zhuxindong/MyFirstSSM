33c4d3f6794ef591d30ad4d818210385953b0584
zxd <501801307@qq.com>
2017-08-26 13:28:00 +0800

    init

---
+++ src/main/java/com/ssm/crud/dao/EmployeeMapper.java
@@ -0,0 +1,38 @@
+package com.ssm.crud.dao;
+
+import com.ssm.crud.pojo.Employee;
+import com.ssm.crud.pojo.EmployeeExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface EmployeeMapper {
+    long countByExample(EmployeeExample example);
+
+    int deleteByExample(EmployeeExample example);
+
+    int deleteByPrimaryKey(Integer empId);
+
+    int insert(Employee record);
+
+    int insertSelective(Employee record);
+
+    List<Employee> selectByExample(EmployeeExample example);
+
+    Employee selectByPrimaryKey(Integer empId);
+
+    int updateByExampleSelective(@Param("record") Employee record, @Param("example") EmployeeExample example);
+
+    int updateByExample(@Param("record") Employee record, @Param("example") EmployeeExample example);
+
+    int updateByPrimaryKeySelective(Employee record);
+
+    int updateByPrimaryKey(Employee record);
+    
+    
+    
+    List<Employee> selectByExampleWithDept(EmployeeExample example);
+    
+    Employee selectByPrimaryKeyWithDept(Integer empId);
+    
+    
+}
